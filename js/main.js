dcf94e0339053c84e0858334855b52b87f3676a2
Cenk Alti <cenkalti@gmail.com>
2014-03-04 12:23:11 -0800

    add linux commands

--- js/main.js
+++ js/main.js
@@ -11,7 +11,7 @@
     error: window.error = console.error.bind(console),
     config: {
       newkontrol: {
-        url: 'ws://kite-kontrol.koding.com'
+        url: 'ws://127.0.0.1:4000/kontrol'
       }
     },
     getSessionToken: function() {
@@ -984,7 +984,7 @@
   (function() {
     var blocks, download, kiteMessage;
     blocks = {
-      download: "$ brew install \"https://kite-cli.s3.amazonaws.com/kite.rb\"\n$ kite register\n$ kite install systeminfo\n$ kite run systeminfo",
+      download: "$ brew install \"https://kite-cli.s3.amazonaws.com/kite.rb\"\n// or, for linux:\n// $ wget https://kite-cli.s3.amazonaws.com/kite-latest-linux.deb\n// $ sudo dpkg -i kite-latest-linux.deb\n$ kite register\n$ kite install systeminfo\n$ kite run systeminfo",
       kiteMessage: "{\n  \"arguments\":[\n    {\n      \"authentication\":{\n        \"key\":\"KpVmfxow4IJCQ3GiRXi3PWu2OMitnDhrLWTrm0TYMqSRwvFL0GQsKiouBL889Iu9\",\n        \"type\":\"kodingKey\"\n      },\n      \"kite\":{\n        \"environment\":\"development\",\n        \"hostname\":\"tardis.local\",\n        \"id\":\"d1a89409-079d-43fc-44d0-999a111c0915\",\n        \"name\":\"application\",\n        \"port\":\"52649\",\n        \"publicIP\":\"\",\n        \"region\":\"localhost\",\n        \"username\":\"devrim\",\n        \"version\":\"1\"\n      },\n      \"withArgs\":{\n        \"environment\":\"\",\n        \"hostname\":\"\",\n        \"id\":\"\",\n        \"name\":\"mathworker\",\n        \"region\":\"\",\n        \"username\":\"devrim\",\n        \"version\":\"\"\n      }\n    },\n    \"[Function]\"\n  ],\n  \"callbacks\":{\n    \"1\":[\n      \"1\"\n    ]\n  },\n  \"links\":[\n\n  ],\n  \"method\":\"getKites\"\n}"
     };
     kiteMessage = document.getElementById('kite-message');
--- src/coffee/main.coffee
+++ src/coffee/main.coffee
@@ -4,6 +4,9 @@ do ->
   blocks =
     download    : """
       $ brew install "https://kite-cli.s3.amazonaws.com/kite.rb"
+      // or, for linux:
+      // $ wget https://kite-cli.s3.amazonaws.com/kite-latest-linux.deb
+      // $ sudo dpkg -i kite-latest-linux.deb
       $ kite register
       $ kite install systeminfo
       $ kite run systeminfo
